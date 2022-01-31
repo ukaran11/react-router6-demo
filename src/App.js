ed59e22edf0ee37b3c8b48e4fb73de41a7ce0910
unknown <ukaran11@gmail.com>
2022-02-01 01:04:28 +0530

    Utsav | Added Home and about routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 
+import { Routes, Route } from 'react-router-dom';
+import { About } from './components/About';
+import { Home } from './components/Home';
+
 function App() {
   return (
-   <h1></h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<About />} />
+    </Routes>
   );
 }
 
