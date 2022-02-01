4274bf0ad66d964c6a5ac5d88013955cfa10b2eb
unknown <ukaran11@gmail.com>
2022-02-01 23:59:43 +0530

    utsav

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { About } from './components/About';
 import { Home } from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+      </Routes>
+    </>
   );
 }
 
