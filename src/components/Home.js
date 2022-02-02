09811546cbb7443e5f9f7af7ba4847feb366a877
unknown <ukaran11@gmail.com>
2022-02-02 22:22:50 +0530

    Utsav | Added Programmatic Navigation to OrderSummary

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Home Page
-        </div>
+        <>
+            <div>
+                Home Page
+            </div>
+            <button onClick={() => navigate('order-summary')}>Place Order</button>
+        </>
     )
 }
---
+++ src/components/OrderSummary.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const OrderSummary = () => {
+  return <div>Order Confirmed!</div>;
+};
+
