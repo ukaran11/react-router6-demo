5c879468e4f50141f0aefcb50bae3249482fcb45
unknown <ukaran11@gmail.com>
2022-02-02 22:06:03 +0530

    Utsav | Added active links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? 'bold' : 'normal'
+    }
+  }
+
   return (
   <div>
       <nav>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
+          <NavLink style={navLinkStyles} to='/'>
+            Home
+          </NavLink>
+          <NavLink style={navLinkStyles} to='/about'>
+            About
+          </NavLink>
       </nav>
   </div>
   );
